5f08513d1fd7e912147875a18a09dc6b05bbb400
Vhvo-2004 <vhvdo.eng22@uea.edu.br>
2025-11-16 19:11:34 -0400

    Fix in deploy

--- database.py
+++ database.py
@@ -2,9 +2,9 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-print("DEBUG => DATABASE_URL =", repr(os.getenv("DATABASE_URL")))
-DATABASE_URL = os.getenv("postgresql://postgreespaic_user:lPCsscFQFH4E6PROLWz50fUHm839w7CV@dpg-d4d0br8gjchc73dmj2u0-a/postgreespaic")  # será INTERNAL_URL no Render
+DATABASE_URL = os.getenv("DATABASE_URL")
+print("DEBUG =>", repr(DATABASE_URL))  # debug seguro
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
