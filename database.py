b09063c2ceecb0c414b2759ec1c5c89b035256a0
Vhvo-2004 <vhvdo.eng22@uea.edu.br>
2025-11-16 19:27:46 -0400

    More logs to Fix deploy

--- database.py
+++ database.py
@@ -5,6 +5,16 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 DATABASE_URL = os.getenv("DATABASE_URL")
 print("DEBUG =>", repr(DATABASE_URL))  # debug seguro
 
+url = os.getenv("DATABASE_URL")
+print("URL =", repr(url))
+try:
+    host = url.split("@")[1].split(":")[0]
+    print("Attempting DNS resolution for:", host)
+    print("Resolved:", socket.gethostbyname(host))
+except Exception as e:
+    print("DNS RESOLUTION ERROR:", e)
+
+
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
