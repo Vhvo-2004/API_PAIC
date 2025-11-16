91752af8f4570bc5b3567d663c6d232a50332a5e
Vhvo-2004 <vhvdo.eng22@uea.edu.br>
2025-11-16 18:45:10 -0400

    Adding logs in deploy

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+print("DEBUG => DATABASE_URL =", repr(os.getenv("DATABASE_URL")))
 DATABASE_URL = os.getenv("postgresql://postgreespaic_user:lPCsscFQFH4E6PROLWz50fUHm839w7CV@dpg-d4d0br8gjchc73dmj2u0-a/postgreespaic")  # será INTERNAL_URL no Render
 
 engine = create_engine(DATABASE_URL)
